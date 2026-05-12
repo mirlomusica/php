2bc5959b6cde34cbbe9afae06e09e8d0835d7a73
Jaume <mirlomusica@gmail.com>
2026-05-12 09:00:54 +0200

    auto mar 12 may 2026 09:00:54 CEST

--- base/base/encuesta_controller.php
+++ base/base/encuesta_controller.php
@@ -6,5 +6,5 @@ $id = filter_input(INPUT_POST, "id");
 
 $query = $conexion->prepare("SELECT * FROM encuesta WHERE codigo=:id");
 $query->execute([":id"=>$id]);
-$resp = $query->fetchAll(PDO::FETCH_ASSOC);
+$resp = $query->fetch(PDO::FETCH_ASSOC);
 echo json_encode($resp);
--- base/base/update_enc.php
+++ base/base/update_enc.php
@@ -3,19 +3,25 @@
 include_once "conexion.php";
 
 $id = filter_input(INPUT_POST, "id");
-$num = filter_input(INPUT_POST,"opcion");
+$num = filter_input(INPUT_POST, "opcion");
 
-switch($num){
+switch ($num) {
     case 1:
-                $query = $conexion->prepare("UPDATE encuesta  SET pregunta1=pregunta1+1 WHERE codigo=:id");
+        $query = $conexion->prepare("UPDATE encuesta  SET pregunta1=pregunta1+1 WHERE codigo=:id");
+        break;
 
     case 2:
-                $query = $conexion->prepare("UPDATE encuesta  SET pregunta2=pregunta2+1 WHERE codigo=:id");
+        $query = $conexion->prepare("UPDATE encuesta  SET pregunta2=pregunta2+1 WHERE codigo=:id");
+        break;
 
     case 3:
-                $query = $conexion->prepare("UPDATE encuesta  SET pregunta2=pregunta3+1 WHERE codigo=:id");
+        $query = $conexion->prepare("UPDATE encuesta  SET pregunta3=pregunta3+1 WHERE codigo=:id");
+        break;
 
 }
-$query->execute([":id"=>$id]);
+$query->execute([":id" => $id]);
 
 
+$query = $conexion->prepare("SELECT * FROM encuesta WHERE codigo=:id");
+$query->execute([":id" => $id]);
+echo json_encode($query->fetch(PDO::FETCH_ASSOC));
